912d1fcd2207a557e1f74fe2c33e66f4805706f0
Shayan Shahbazi <upm200@gmail.com>
2026-09-12 00:28:25 +0330

    1.0.0

---
+++ blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    ordering = ('title', '-publish')
+    search_fields = ('title', 'content')
+    list_filter = ('publish', 'status')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ['title']}
+    list_editable = ['status']
+    list_display_links = ('title', 'author')
